6d23e144c74b14455369dd48637d392aac70615d
Harald Ringvold <harald.ringvold@vegvesen.no>
2016-02-26 14:21:37 +0100

    Next dev version

--- cmd/version.go
+++ cmd/version.go
@@ -32,7 +32,7 @@ import (
 )
 
 const VersionNumber = 0.2
-const VersionSuffix = "" // blank this when doing a releas
+const VersionSuffix = "-DEV" // blank this when doing a releas
 
 var (
 	CommitHash string
