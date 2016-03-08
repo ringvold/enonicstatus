95cdfba1c46e44aa1074684616daacb3a28234d1
Harald Ringvold <exthxr@vegvesen.no>
2016-03-08 11:00:43 +0100

    Correctly format uptime

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -23,72 +23,75 @@ var headerLinePrefix string = "# "
 type PlainFormatter struct {
 }
 
-func (p PlainFormatter) HostName(s string) string {
-	return fmt.Sprint(headerLinePrefix, s)
+func (p PlainFormatter) HostName(name string) string {
+	return fmt.Sprint(headerLinePrefix, name)
 }
 
-func (p PlainFormatter) IndexStatus(s string) string {
-	return fmt.Sprint(linePrefix, "Index: ", s)
+func (p PlainFormatter) IndexStatus(index string) string {
+	return fmt.Sprint(linePrefix, "Index: ", index)
 }
 
-func (p PlainFormatter) Master(s string) string {
-	return fmt.Sprint(linePrefix, "Master: ", s)
+func (p PlainFormatter) Master(master string) string {
+	return fmt.Sprint(linePrefix, "Master: ", master)
 }
 
-func (p PlainFormatter) NodesSeen(s float64) string {
-	return fmt.Sprint(linePrefix, "Nodes seen: ", s)
+func (p PlainFormatter) NodesSeen(nodesSeen float64) string {
+	return fmt.Sprint(linePrefix, "Nodes seen: ", nodesSeen)
 }
 
-func (p PlainFormatter) Uptime(s float64) string {
-	return fmt.Sprint(linePrefix, "Uptime: ", s)
+func (p PlainFormatter) Uptime(uptime float64) string {
+	uptimeString := strconv.FormatFloat(uptime, 'f', -1, 64)
+	duration := fmt.Sprintf("%sms", uptimeString)
+	formattedUptime, _ := time.ParseDuration(duration)
+	return fmt.Sprint(linePrefix, "Uptime: ", formattedUptime)
 }
 
-func (p PlainFormatter) Version(s string) string {
-	return fmt.Sprint(linePrefix, "Version: ", s)
+func (p PlainFormatter) Version(version string) string {
+	return fmt.Sprint(linePrefix, "Version: ", version)
 }
 
 type TerminalFormatter struct {
 }
 
-func (p TerminalFormatter) HostName(s string) string {
-	return fmt.Sprint(headerLinePrefix, s)
+func (p TerminalFormatter) HostName(name string) string {
+	return fmt.Sprint(headerLinePrefix, name)
 }
 
-func (p TerminalFormatter) IndexStatus(s string) string {
+func (p TerminalFormatter) IndexStatus(index string) string {
 	formatting := ""
-	if s == "GREEN" {
+	if index == "GREEN" {
 
 		formatting = "@g"
 	}
-	if s == "YELLOW" {
+	if index == "YELLOW" {
 		formatting = "@y"
 	}
-	if s == "RED" {
+	if index == "RED" {
 		formatting = "@r"
 	}
-	return color.Sprint(linePrefix, "Index:", formatting, s)
+	return color.Sprint(linePrefix, "Index:", formatting, index)
 }
 
-func (p TerminalFormatter) Master(s string) string {
+func (p TerminalFormatter) Master(master string) string {
 	formatting := ""
-	if s == "true" {
+	if master == "true" {
 		formatting = "@g"
 	}
-	return color.Sprint(linePrefix, "Master:", formatting, s)
+	return color.Sprint(linePrefix, "Master:", formatting, master)
 }
 
-func (p TerminalFormatter) NodesSeen(s float64) string {
-	return fmt.Sprint(linePrefix, "Nodes seen: ", s)
+func (p TerminalFormatter) NodesSeen(nodesSeen float64) string {
+	return fmt.Sprint(linePrefix, "Nodes seen: ", nodesSeen)
 }
 
-func (p TerminalFormatter) Uptime(s float64) string {
-	uptimeString := strconv.FormatFloat(s, 'f', -1, 64)
+func (p TerminalFormatter) Uptime(uptime float64) string {
+	uptimeString := strconv.FormatFloat(uptime, 'f', -1, 64)
 	duration := fmt.Sprintf("%sms", uptimeString)
 	formattedUptime, _ := time.ParseDuration(duration)
 	formatting := "@b"
 	return color.Sprint(linePrefix, "Uptime:", formatting, formattedUptime)
 }
 
-func (p TerminalFormatter) Version(s string) string {
-	return fmt.Sprint(linePrefix, "Version: ", s)
+func (p TerminalFormatter) Version(version string) string {
+	return fmt.Sprint(linePrefix, "Version: ", version)
 }
